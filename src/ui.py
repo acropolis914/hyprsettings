150b2fcbf0df45d0011284e2f0c571d5945047a9
acropolis914 <hed-phsuarnaba@smu.edu.ph>
2025-12-01 02:36:10 +0800

    safer search of fonts

--- src/ui.py
+++ src/ui.py
@@ -32,10 +32,13 @@ global daemon
 global verbose
 
 _original_print = builtins.print
+
+
 def print(*args, **kwargs):
 	_original_print("[ui-backend]", *args, **kwargs)
 
-def log(msg, prefix= "LOG", only_verbose=False):
+
+def log(msg, prefix="LOG", only_verbose=False):
 	if verbose and only_verbose:
 		print(f"[{prefix}] {msg}")
 
@@ -43,6 +46,7 @@ def log(msg, prefix= "LOG", only_verbose=False):
 def on_loaded(window):
 	print("DOM is ready")
 	window.events.loaded -= on_loaded
+
 
 def on_closed(window):
 	if daemon:
@@ -52,35 +56,48 @@ def on_closed(window):
 	print("Window closed. Terminating process.")
 	os._exit(0)
 
+
 # ------------------- Socket toggle functions -------------------
 def handle_client(conn):
 	global window_instance, window_visible
 	try:
 		data = conn.recv(1024)
 		if data.decode() == "TOGGLE" and window_instance:
-
-			activeworkspace = subprocess.run(
-				  "hyprctl monitors -j | jq '.[] | select(.focused==true) | .activeWorkspace.name'",
-				  shell=True, capture_output=True, text=True
-			).stdout.strip().strip('\"')
+			activeworkspace = (
+				subprocess.run(
+					"hyprctl monitors -j | jq '.[] | select(.focused==true) | .activeWorkspace.name'",
+					shell=True,
+					capture_output=True,
+					text=True,
+				)
+				.stdout.strip()
+				.strip('"')
+			)
 			# log(f"Active workspace from focused monitor: '{activeworkspace}'")
 
-			specialworkspace = subprocess.run(
-				  "hyprctl monitors -j | jq '.[] | select(.focused==true) | .specialWorkspace.name'",
-				  shell=True, capture_output=True, text=True
-			).stdout.strip().strip("\"")
+			specialworkspace = (
+				subprocess.run(
+					"hyprctl monitors -j | jq '.[] | select(.focused==true) | .specialWorkspace.name'",
+					shell=True,
+					capture_output=True,
+					text=True,
+				)
+				.stdout.strip()
+				.strip('"')
+			)
 			# log(f"Special workspace from focused monitor: '{specialworkspace}'")
 
-			if specialworkspace :
+			if specialworkspace:
 				log(f"Special workspace {specialworkspace} detected, overriding active workspace")
 				activeworkspace = specialworkspace
 			# log(f"Workspace to target: '{activeworkspace}'")
 
-
 			# get hyprsettings window workspace
 			hyprsettings_window_workspace_name = subprocess.run(
-				  'hyprctl -j clients | jq -r \'.[] | select(.initialClass=="ui.py") | .workspace.name\'',
-				  shell=True, capture_output=True, text=True
+				"hyprctl -j clients | jq -r '.[] | select(.initialClass==\"ui.py\") | .workspace.name'",
+				shell=True,
+				capture_output=True,
+				text=True,
 			).stdout.strip()
 			# log(f"HyprSettings window currently on workspace: '{hyprsettings_window_workspace_name}'")
 
@@ -91,10 +108,13 @@ def handle_client(conn):
 				window_visible = False
 
 			elif window_visible and activeworkspace != hyprsettings_window_workspace_name:
-				log(f"Window visible BUT on different workspace ({hyprsettings_window_workspace_name}) → moving")
+				log(
+					f"Window visible BUT on different workspace ({hyprsettings_window_workspace_name}) → moving"
+				)
 				move = subprocess.run(
-					  ["hyprctl", "dispatch", "movetoworkspace", f"{activeworkspace},initialclass:ui.py"],
-					  capture_output=True, text=True
+					["hyprctl", "dispatch", "movetoworkspace", f"{activeworkspace},initialclass:ui.py"],
+					capture_output=True,
+					text=True,
 				)
 				# log(f"Move stdout: '{move.stdout.strip()}'")
 				# log(f"Move stderr: '{move.stderr.strip()}'")
@@ -108,6 +128,7 @@ def handle_client(conn):
 	finally:
 		conn.close()
 
+
 def start_socket_server():
 	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 		s.bind((HOST, PORT))
@@ -115,6 +136,7 @@ def start_socket_server():
 		while True:
 			conn, addr = s.accept()
 			threading.Thread(target=handle_client, args=(conn,), daemon=True).start()
+
 
 def send_toggle():
 	try:
@@ -177,11 +199,16 @@ class Api:
 
 		window_config_path = Path.home() / ".config" / "hypr" / "hyprsettings.toml"
 		template = Path(thisfile_path / "default_config.toml")
-		temporary_font = self.list_fonts(mono=False, nerd=True)[1]
+		temporary_font = None
+		try:
+			temporary_font = self.list_fonts(mono=False, nerd=True)[1]
+		except IndexError:
+			print("No nerd font found. Using monospace.")
+			temporary_font = None
 
 		if not window_config_path.is_file() or window_config_path.stat().st_size == 0:
 			print(f"Config file not found in {window_config_path}")
-			with open( 
+			with open(
 				template,
 				"r",
 			) as default_config:
@@ -205,24 +232,24 @@ class Api:
 				self.window_config = config
 				version_migration()
 				return self.window_config
+
 	def get_builtin_themes(self):
-		file_path = thisfile_path/"ui"/"themes_builtin"
-		themes=[]
+		file_path = thisfile_path / "ui" / "themes_builtin"
+		themes = []
 		for file in os.listdir(file_path):
-			theme_file= Path(file_path/file)
+			theme_file = Path(file_path / file)
 			if not theme_file.is_file():
 				continue
 			if not str(theme_file).endswith(".toml"):
 				continue
 			with open(theme_file, "r", encoding="utf-8") as theme:
-				file_content=theme.read()
+				file_content = theme.read()
 
 				theme_content = toml.parse(file_content)
 				# print(f'Theme content: {theme_content}')
-				for theme_definition in theme_content.get("theme",[]):
+				for theme_definition in theme_content.get("theme", []):
 					themes.append(theme_definition)
 		return themes
-
 
 	def save_window_config(self, json_fromjs, part="config"):
 		print(f"Called save window {part}")
@@ -245,12 +272,9 @@ class Api:
 		return [f.strip() for f in result.stdout.splitlines() if f.strip()]
 
 
-
 # ------------------- Main entry -------------------
 if __name__ == "__main__":
-	parser = argparse.ArgumentParser(
-		  description="A loyal hyprland parser and gui editor for hyprland.conf"
-	)
+	parser = argparse.ArgumentParser(description="A loyal hyprland parser and gui editor for hyprland.conf")
 	parser.add_argument("-d", "--daemon", action="store_true", help="Run in background started for quick startup")
 	parser.add_argument("-v", "--verbose", action="store_true", help="Get more descriptive logs")
 	args = parser.parse_args()
@@ -260,7 +284,7 @@ if __name__ == "__main__":
 
 	threading.Thread(target=start_socket_server, daemon=args.daemon).start()
 	daemon = args.daemon
-	verbose= args.verbose
+	verbose = args.verbose
 	if args.daemon:
 		print("Started HyprSettings in daemon mode. Initially hiding window")
 
@@ -268,27 +292,27 @@ if __name__ == "__main__":
 	mimetypes.add_type("application/javascript", ".js")
 	print(Path(thisfile_path / "ui" / "index.html"))
 	window_instance = webview.create_window(
-		  "HyprSettings",
-		  "ui/index.html",
-		  js_api=api,
-		  transparent=True,
-		  width=800,
-		  height=600,
-		  easy_drag=True,
-		  min_size=(400, 300),
-		  hidden=args.daemon
+		"HyprSettings",
+		"ui/index.html",
+		js_api=api,
+		transparent=True,
+		width=800,
+		height=600,
+		easy_drag=True,
+		min_size=(400, 300),
+		hidden=args.daemon,
 	)
 	window_visible = not args.daemon
 	window_instance.events.loaded += on_loaded
 	window_instance.events.closed += on_closed
 
 	# if args.daemon:
-		# window_instance.hide()
+	# window_instance.hide()
 
 	webview.start(
-		  gui="gtk",
-		  debug=True,
-		  private_mode=True,
-		  storage_path=str(cache_path),
-		  icon="icon-48.png",
+		gui="gtk",
+		debug=True,
+		private_mode=True,
+		storage_path=str(cache_path),
+		icon="icon-48.png",
 	)
