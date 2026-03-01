cd1e3c9964f3c35069ce29510d696a6c2ca25e82
acropolis914 <hed-phsuarnaba@smu.edu.ph>
2026-03-02 04:06:01 +0800

    feat: enhance hyprsettings.sh to download fresh script version and ensure cleanup
    Signed-off-by: acropolis914 <hed-phsuarnaba@smu.edu.ph>

--- default.nix
+++ default.nix
@@ -34,7 +34,7 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "hyprsettings";
-  version = "0.9.1.74";
+  version = "0.9.1.75";
 
   src = ./.;
 
--- hyprsettings.sh
+++ hyprsettings.sh
@@ -1,10 +1,21 @@
 #!/bin/sh
+
 LOCAL_PY="./hyprsettings.py"
 TMP_PY="/tmp/hyprsettings_installer.py"
+
 if [ -f "$LOCAL_PY" ]; then
     TARGET_PY="$LOCAL_PY"
 else
+    # Ensure we start with a clean slate
+    rm -f "$TMP_PY"
+
+    # Download the fresh version
     curl -sL "https://github.com/acropolis914/hyprsettings/raw/master/hyprsettings.py" -o "$TMP_PY"
     TARGET_PY="$TMP_PY"
+
+    # Cleanup: Delete the temp file when this script exits
+    trap 'rm -f "$TMP_PY"' EXIT
 fi
+
+# Execute Python and pass all arguments ($@)
 exec python3 "$TARGET_PY" "$@" </dev/tty
