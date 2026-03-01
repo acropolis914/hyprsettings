b31fdfe7211ca9a3ee7810b2e771d1bb1c62c113
acropolis914 <hed-phsuarnaba@smu.edu.ph>
2026-03-02 03:48:48 +0800

    feat: modify hyprsettings.sh to read from /dev/tty for interactive input
    Signed-off-by: acropolis914 <hed-phsuarnaba@smu.edu.ph>

--- default.nix
+++ default.nix
@@ -34,7 +34,7 @@ let
 in
 stdenv.mkDerivation rec {
   pname = "hyprsettings";
-  version = "0.9.1.70";
+  version = "0.9.1.71";
 
   src = ./.;
 
--- hyprsettings.sh
+++ hyprsettings.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 TMP_INSTALLER="/tmp/hyprsettings_installer.py"
 curl -sL https://github.com/acropolis914/hyprsettings/raw/master/hyprsettings.py -o "$TMP_INSTALLER"
-exec python3 "$TMP_INSTALLER" "$@"
+
+python3 "$TMP_INSTALLER" "$@" </dev/tty
