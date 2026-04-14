2353028ab4fd574eb58790df7f66d4459b329266
acropolis914 <hed-phsuarnaba@smu.edu.ph>
2026-04-14 08:34:55 +0800

    Dummy commit to manually trigger version hook

--- src/ui-src/scripts/ui_components/switchBox.ts
+++ src/ui-src/scripts/ui_components/switchBox.ts
@@ -1,17 +1,13 @@
 export function createSwitchBox(defaultValue: boolean) {
 	const wrapper = document.createElement('label')
 	wrapper.classList.add('switch')
-
 	const checkbox = document.createElement('input')
 	checkbox.type = 'checkbox'
 	checkbox.tabIndex = -1
 	checkbox.checked = defaultValue
-
 	const fakeSlider = document.createElement('span')
 	fakeSlider.className = 'fake-slider'
-
 	wrapper.appendChild(checkbox)
 	wrapper.appendChild(fakeSlider)
-
 	return { wrapper, checkbox }
 }
